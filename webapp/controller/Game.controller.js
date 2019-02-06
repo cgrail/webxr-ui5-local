550fa4b5ca3afb96549e8aa8a55e9e410b5798fa
Christian Grail <christian.grail@sap.com>
2019-02-06 14:12:22 +0100

    Formatting

--- webapp/controller/Game.controller.js
+++ webapp/controller/Game.controller.js
@@ -3,16 +3,16 @@ sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/m/MessageToast",
 	"webar-test/assets/ArAssets"
-], function(Controller, MessageToast, ArAssets) {
+], function (Controller, MessageToast, ArAssets) {
 	"use strict";
 
 	return Controller.extend("webar-test.controller.Game", {
-		onInit: function() {
+		onInit: function () {
 			this.arView = this.byId("arView");
 			this.assets = new ArAssets();
 		},
 
-		shoot: function() {
+		shoot: function () {
 			MessageToast.show("Please implement me...");
 		}
 	});
